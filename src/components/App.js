742c54e318cc03c1dc903669500761b2a6815378
Filipe Valente Gomes <Filipe@Filipes-iMac.local>
2017-08-22 17:02:30 -0400

    added home header and footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// COMPONENTS
+import Header from './header';
+import Footer from './footer';
+import Home from './home';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        hello
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route excact path='/' component={Home} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
